c7496fe27629a9a2300bebe2e813022109ac9d0a
Come De Goeyse <cdegoeyse@emi.u-bordeaux.fr>
2025-10-10 11:07:07 +0200

    Semi working baby example

--- create_data.py
+++ create_data.py
@@ -7,9 +7,15 @@ def save_data(X, y, iter):
     np.savez(f"datas/data_iter_{iter}.npz", X_all=X, y_all=y)
 
 # centers = nombre de clusters, et y1 y recupere l'etiquette correspondant (0, 1 ou 2 en ft du cluster), n_features = nombre de dimensions, random_state = permet d'avoir tjrs le meme resultat quand on lance le code (42 est la convention)
-def new_points(n_samples: int, std_noise: float, centers: int, n_features: int,  cluster_std: float, seed: int|None|np.random.RandomState = None):
+def new_points(n_samples: int,
+               pct_noise: float,
+               centers: int,
+               n_features: int,
+               cluster_std: float,
+               seed: int|None|np.random.RandomState = None
+               ):
     """
-    
+    Summary TODO
 
     Args:
         n_samples (int): Total numbers of points
@@ -27,7 +33,9 @@ def new_points(n_samples: int, std_noise: float, centers: int, n_features: int, 
     # cluster_std a tester : [0.5, 1.0, 2.0], noise_std a tester : 0.5 Ã— cluster_std (faible bruit relatif), 1.0 Ã— cluster_std (bruit comparable), 3.0 Ã— cluster_std (bruit fort), 5.0 Ã— cluster_std (bruit tres fort / outliers)
 
     # Pour tester la robustesse : Si tu veux tester robustesse faible â†’ 500 (3%). Pour robustesse moderee â†’ 1500 (â‰ˆ10%). Pour robustesse forte â†’ 3000 (â‰ˆ20%).
-    noise = np.random.normal(0, std_noise, (n_samples, n_features))
+    noise = np.random.normal(0,
+                             pct_noise*cluster_std, 
+                             (n_samples, n_features)) # Multiplying cluster std to get a percentage of noise ?
 
 
     # ajout donnees normales + bruit
